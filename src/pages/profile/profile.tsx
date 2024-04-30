18e2d4651f5ca0797436e2912b6c28b39cabf3ed
JayIvanOctat <reazon07@gmail.com>
2024-04-30 09:29:52 +0800

    added a note

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,70 +1,12 @@
 import React, { useState } from 'react';
-import {
-  IonBackButton,
-  IonButton,
-  IonButtons,
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
-  IonCol,
-  IonContent,
-  IonGrid,
-  IonHeader,
-  IonIcon,
-  IonInput,
-  IonItem,
-  IonLabel,
-  IonList,
-  IonPage,
-  IonRow,
-  IonTitle,
-  IonToolbar,
-  IonItemDivider,
-  IonSearchbar,
-  IonAlert,
-  IonActionSheet,
-  IonAvatar,
-  IonThumbnail
-} from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonCard, IonCardHeader, IonCardTitle, 
+  IonCardSubtitle, IonCardContent, IonAlert } from '@ionic/react';
 import './profile.css';
 
-//Dynamic data reference
-import rizzCard from '../../assets/json/rizzCard.json';
-
-const Profile: React.FC = () => {
 
 
-  const [showAlert, setShowAlert] = useState(false);
-  const [randomIndex, setRandomIndex] =  useState<number | null>(null); // State to store random index
-
-  // Function to generate a random index
-  const generateRandomIndex = () => {
-    return Math.floor(Math.random() * rizzCard.length);
-  };
-
-  // Function to generate a random message
-  const renderRandomMessage = () => {
-    if (randomIndex !== null) {
-      return rizzCard[randomIndex].message;
-    } else {
-      return ''; // Return empty string if randomIndex is null
-    }
-  };
-
-  // Function to handle opening of the alert
-  const handleOpenAlert = () => {
-    const newIndex = generateRandomIndex();
-    setRandomIndex(newIndex);
-    setShowAlert(true);
-  };
-
-  // Function to handle closing of the alert
-  const handleAlertDismiss = () => {
-    setRandomIndex(0); // Reset the index to 0
-    setShowAlert(false); // Hide the alert
-  };
+const profile: React.FC = () => {
+  const [clickCount, setClickCount] = useState(0);
 
   
   return (
@@ -118,4 +60,4 @@ const Profile: React.FC = () => {
   );
 };
 
-export default Profile;
+export default profile;
